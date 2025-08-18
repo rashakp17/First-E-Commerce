ab06755ef0e26d6aba86eeb457e0cb33cb406f54
rashakp17 <rashaafii2007@gmail.com>
2025-08-18 16:25:14 +0530

    base

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     rollupOptions: {
       external: ['react-redux']
+      
     }
   }
 });
